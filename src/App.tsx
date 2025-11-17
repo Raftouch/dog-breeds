e5c391d6472504b209104a4510fa495b2b4d9ce3
raftouch <rafanelkin@gmail.com>
2025-11-17 16:17:48 +0100

    add dark/light mode toggler logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import BreedList from "./components/BreedList";
 
 function App() {
-  return <BreedList />;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const theme = isDarkMode ? "dark-mode" : "";
+
+  const handleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  return (
+    <div className={theme}>
+      <button className="theme-toggler" onClick={handleTheme}>
+        Light Dark Mode
+      </button>
+      <BreedList />
+    </div>
+  );
 }
 
 export default App;
