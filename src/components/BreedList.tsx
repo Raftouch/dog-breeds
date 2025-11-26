8cb50b43334e2f98fad41a5e714ec176b319b396
raftouch <rafanelkin@gmail.com>
2025-11-26 17:17:59 +0100

    add error and loading handling

--- src/components/BreedList.tsx
+++ src/components/BreedList.tsx
@@ -7,20 +7,37 @@ import { dogApi } from "../utils/api";
 export default function BreedList() {
   const [dogBreeds, setDogBreeds] = useState<[string, string[]][]>([]);
   const [search, setSearch] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const getDogBreeds = async () => {
+    setLoading(true);
+    setError(null);
+
     try {
       const res = await fetch(`${dogApi}/breeds/list/all`);
 
-      if (!res.ok) throw new Error("Failed to fetch breeds");
+      // if (!res.ok) throw new Error("Failed to fetch breeds");
+      if (!res.ok) {
+        setError("Failed to fetch breeds");
+        return;
+      }
 
       const data: DogBreedData = await res.json();
       const breedEntries = Object.entries(data.message);
 
-      data.status === "success" ? setDogBreeds(breedEntries) : setDogBreeds([]);
+      if (data.status === "success") {
+        setDogBreeds(breedEntries);
+      } else {
+        setError("Failed to fetch");
+        setDogBreeds([]);
+      }
     } catch (error) {
       console.error(error);
+      setError("Something went wrong");
       setDogBreeds([]);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -31,6 +48,9 @@ export default function BreedList() {
   const filteredBreeds = dogBreeds.filter((breed) => {
     return breed[0].toLowerCase().includes(search.toLowerCase());
   });
+
+  if (loading) return <>Loading...</>;
+  if (error) return <>{error}</>;
 
   return (
     <>
