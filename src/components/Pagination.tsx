f516c7b7e3efaad40286146a3f0a8484ed428d13
raftouch <rafanelkin@gmail.com>
2025-11-30 14:42:42 +0100

    add and integrate function to generate dynamic pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -11,6 +11,35 @@ interface PaginationProps {
   currentPage: number;
 }
 
+type PageItem = number | "...";
+
+const getPages = (currentPage: number, totalPages: number): PageItem[] => {
+  if (totalPages <= 1) return [1];
+
+  const pages: PageItem[] = [];
+
+  const first = 1;
+  const last = totalPages;
+
+  const prev = currentPage - 1;
+  const next = currentPage + 1;
+
+  pages.push(first);
+
+  if (prev > first + 1) pages.push("...");
+
+  if (prev > first) pages.push(prev);
+
+  if (currentPage !== first && currentPage !== last) pages.push(currentPage);
+
+  if (next < last) pages.push(next);
+  if (next < last - 1) pages.push("...");
+
+  pages.push(last);
+
+  return pages;
+};
+
 export default function Pagination({
   page,
   totalPages,
@@ -21,10 +50,12 @@ export default function Pagination({
   setCurrentPage,
   currentPage,
 }: PaginationProps) {
-  let pages = [];
-  for (let i = 1; i <= Math.ceil(totalResults / resultsPerPage); i++) {
-    pages.push(i);
-  }
+  //   let pages = [];
+  //   for (let i = 1; i <= Math.ceil(totalResults / resultsPerPage); i++) {
+  //     pages.push(i);
+  //   }
+
+  const pages = getPages(page, totalPages);
 
   return (
     <div className={styles.pagination}>
@@ -32,8 +63,10 @@ export default function Pagination({
         Prev
       </button>
 
-      {pages.map((p, index) => {
-        return (
+      {pages.map((p, index) =>
+        p === "..." ? (
+          <span key={index}>...</span>
+        ) : (
           <button
             className={p === currentPage ? styles.active : ""}
             key={index}
@@ -41,8 +74,8 @@ export default function Pagination({
           >
             {p}
           </button>
-        );
-      })}
+        )
+      )}
 
       <button disabled={page === totalPages} onClick={onNext}>
         Next
